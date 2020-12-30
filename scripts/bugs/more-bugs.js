90b3e503907d452c03f910c32cbefbc291dc686e
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2020-12-30 20:21:33 +0300

    Create more-bugs.js
    I added some old stuff for ya...

---
+++ scripts/bugs/more-bugs.js
@@ -0,0 +1,9 @@
+//V3.1 Reactor
+Blocks.thoriumReactor.heating = -1;
+//V5 Scorch
+Blocks.scorch.reloadTime =0
+//V5 Plast Wall
+Blocks.plastaniumWall.insulated = false;
+Blocks.plastaniumWall.absorbLasers = false;
+Blocks.plastaniumWallLarge.insulated = false;
+Blocks.plastaniumWallLarge.absorbLasers = false;
